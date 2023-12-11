b77ac6589e9e720a1a3af875592ebd7f3233a749
dongsoo cho <soosoft@daum.net>
2023-12-11 09:24:17 +0900

    update

--- jcar.py
+++ jcar.py
@@ -1,14 +1,15 @@
 import pygame
 import sys
+import math
+
 pygame.init()
 pygame.display.set_caption("Moving Car in Pygame")
+
 WIDTH, HEIGHT = 1024, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font = pygame.font.SysFont("malgungothic", 24) 
+move_sound = pygame.mixer.Sound('sound/move.mp3')
 
-
-
-import math
 
 class Car:
     def __init__(self, image, speed, X, Y):
@@ -16,9 +17,8 @@ class Car:
         self.original_image = pygame.transform.scale(original_image, (original_image.get_width()*0.9, original_image.get_height()*0.9))
         self.image = self.original_image
         self.location = self.image.get_rect(center=(X, Y))
-        self.speed = speed
+        self.speed = 0
         self.angle = 90
-        self.movement = 0
         self.direction="stop"
 
     def rotate(self):
@@ -28,11 +28,21 @@ class Car:
 
     def move(self):
         # 라디안으로 각도 변환
+        if car.direction=="stop":
+            self.speed =0
+            move_sound.stop()
+        if car.direction=="forward":
+            self.speed +=1 
+            move_sound.play()
+
+        if car.direction=="back": 
+            self.speed -=0.5
+            move_sound.stop()
+
         radians = math.radians(self.angle)
-        self.location.x += self.movement * math.cos(radians)
-        self.location.y -= self.movement * math.sin(radians)
-        if car.direction=="forward":  self.movement +=2
-        if car.direction=="back":  self.movement -=0.5
+        self.location.x += self.speed * math.cos(radians)
+        self.location.y -= self.speed * math.sin(radians)
+        
 
 car = Car('carA1.png', 1, WIDTH/2, HEIGHT-100)
 
@@ -49,22 +59,17 @@ while True:
                     car.angle -= 10  # 오른쪽으로 회전
                 case (pygame.K_UP):
                     car.direction="forward"
-                    car.movement = car.speed  # 전진
                 case (pygame.K_DOWN):
                     car.direction="back"
-                    car.movement = -car.speed  # 후진
                 case (pygame.K_SPACE):
-                    car.movement = 0  # 멈춤
-                    car.speed =1
+                    car.speed =0
                     car.direction="stop"
-
-    if  not car.direction=="stop":
-        car.move()
-        
+    car.move()
     car.rotate()
 
     screen.fill((0, 0, 0))
     screen.blit(car.image, car.location)
+    screen.blit(font.render("speed = " + str(car.speed), True, (255,255,255)), (50, 50))
 
     pygame.display.update()
     pygame.time.Clock().tick(30)
--- sm298-04.py
+++ sm298-04.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import sys
 import time  
+import pygame_gui
 # Pygame 초기화
 pygame.init()
 # 상수들
@@ -14,10 +15,14 @@ angle = 45  # 출발 각도
 speed = 30  # 공의 속도
 floor_friction = 0.9  # 바닥 마찰 계수
 
+
+# 속도 계산
 dx = speed * math.cos(math.radians(angle))  # x 축의 속도
 dy = -speed * math.sin(math.radians(angle))  # y 축의 속도
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+font = pygame.font.SysFont("malgungothic", 24) 
+pygame.display.set_caption("Physics Simulation")
 
 # 클래스 
 class Ball:
@@ -26,27 +31,43 @@ class Ball:
         self.y=Y
         self.dx=DX
         self.dy=DY
-        self.pause=True
+        self.distance=0
+        self.stop=True
+
     def move(self):
         # 위치 변화
         self.dy += gravity
         self.x += self.dx
         self.y += self.dy
 
-        # 왼쪽 & 오른쪽 벽 충돌
-        if self.x + R > WIDTH:
-            self.x = WIDTH - R
-            self.dx *= -restitution
-        elif self.x - R < 0:
-            self.x = R
-            self.dx *= -restitution
-        # 상단 & 하단 벽 충돌
-        if self.y + R > HEIGHT:
-            self.y = HEIGHT - R
-            self.dy *= -restitution
-        elif self.y - R < 0:
-            self.y = R
-            self.dy *= -restitution
+        
+        # 충돌 조건 계산
+        collide_right = self.x + R > WIDTH
+        collide_left = self.x - R < 0
+        collide_bottom = self.y + R > HEIGHT
+        collide_top = self.y - R < 0
+
+        # 충돌 처리
+        match (collide_right, collide_left, collide_bottom, collide_top):
+            case (True, _, _, _):  # 오른쪽 벽 충돌
+                self.x = WIDTH - R
+                self.dx *= -restitution
+            case (_, True, _, _):  # 왼쪽 벽 충돌
+                self.x = R
+                self.dx *= -restitution
+            case (_, _, True, _):  # 아래쪽 벽 충돌
+                self.y = HEIGHT - R
+                self.dy *= -restitution
+                self.dx *= floor_friction  # 바닥 마찰 계수 적용
+                if ball.distance==0 :
+                    ball.distance=self.x
+                    print(f"distance :{self.x:.2f}")
+
+            case (_, _, _, True):  # 위쪽 벽 충돌
+                self.y = R
+                self.dy *= -restitution
+            case _:
+                pass  # 충돌이 없는 경우
 
 
     def display(self):
@@ -55,10 +76,12 @@ class Ball:
 
 
 ball = Ball(X, Y,  dx, dy)
-
+screen.fill((220, 220, 220))
+ball.display()
+pygame.display.flip()
 
 running = True
-
+print(  ball.stop)
 while running:
     
 
@@ -69,17 +92,16 @@ while running:
         elif event.type == pygame.KEYDOWN:
             print(" Press ascii code=",event.key )
             if event.key == pygame.K_SPACE:
-                 ball.pause=True  if  ball.pause !=True  else False
+                ball.stop = not ball.stop
 
     
 
     screen.fill((220, 220, 220))
-    
-    if not ball.pause:
+    if not ball.stop:
         ball.move()
 
     ball.display()
-
+    screen.blit(font.render("distance = " + str(ball.distance), True, (255, 0, 0)), (50, 50))
     pygame.display.flip()
     pygame.time.Clock().tick(30)
 
