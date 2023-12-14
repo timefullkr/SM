7c6865c277ea9b1f5986478c4b2afb6ee0c9f39b
aaa <timefullkr@naver.com>
2023-12-14 13:17:21 +0900

    updata

--- pygameTimer.py
+++ pygameTimer.py
@@ -3,19 +3,32 @@ import sys
 
 pygame.init()
 pygame.display.set_caption("Pygame Timer")
-screen = pygame.display.set_mode((800, 600))
-font = pygame.font.SysFont("malgungothic", 80) # 시스템 폰트 사용 시
+screen = pygame.display.set_mode((200, 100))
+font = pygame.font.SysFont("malgungothic", 30) 
 
+cnt = 0
+last_update_time = 0 
 while True:
+    # 현재 시간을 밀리초 단위로 가져옴
+    current_time = pygame.time.get_ticks()  
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    
+    if current_time - last_update_time > 1000:
+       cnt +=1; last_update_time = current_time  
+ 
     screen.fill((0, 0, 0))
-    screen.blit( font.render("Timer : 0", True, (255, 255, 255)), [150, 200])
+    screen.blit(font.render(
+                        f"Timer : {cnt}",
+                        True,
+                        (255, 255, 255)
+                        ),
+                        [30,30]
+                )
 
     pygame.display.update()
     pygame.time.Clock().tick(30)
-        
